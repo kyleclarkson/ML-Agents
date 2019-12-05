57a31615211a5b687241a32780cf011c69f9ed2d
Kyle Clarks <kyleclarkson17@hotmail.com>
2019-12-05 14:03:55 -0800

    Started PushBlock example.
    Added folders, started on scripts.

---
+++ ML-Agents-Examples/Assets/ML-Agents/Examples-Redone/PushBlock/Scripts/GoalDetect_ex.cs
@@ -0,0 +1,17 @@
+﻿
+
+using UnityEngine;
+
+
+public class GoalDetect_ex : MonoBehaviour {
+
+    public PushAgentBasic_ex agent;
+
+    private void OnCollisionEnter(Collision collision) {
+
+        // Block has entered goal area. 
+        if (collision.gameObject.CompareTag("goal")) {
+            // TODO
+        }
+    }
+}
---
+++ ML-Agents-Examples/Assets/ML-Agents/Examples-Redone/PushBlock/Scripts/PushAgentBasic_ex.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class PushAgentBasic_ex : Agent {
+
+    // The surface of the game area. 
+    public GameObject ground;
+    public GameObject area;
+
+    [HideInInspector]
+    public Bounds areaBounds;
+
+
+    PushBlockAcademy_ex m_Academy;
+
+    // Goal to which block needs to be moved to.
+    public GameObject goal;
+    // The block that needs to be moved.
+    public GameObject block;
+
+    // Detects when block reaches the goal area. 
+    [HideInInspector]
+    public GoalDetect_ex goalDetect;
+
+    // Used to change ground material given success or failure of agent.
+    Renderer m_GroundRenderer;
+    Material m_GroundMaterial;
+
+    Rigidbody m_BlockRb;
+    Rigidbody m_AgentRb;
+    RayPerception m_RayPerception;
+
+    float[] m_RayAngles = { 0f, 45f, 70f, 90f, 110f, 135f, 180f };
+    string[] m_DectableObjects = { "block", "goal", "wall" };
+
+
+    void Awake() {
+        m_Academy = FindObjectOfType<PushBlockAcademy_ex>();    
+    }
+
+    public override void InitializeAgent() {
+        base.InitializeAgent();
+
+        // Setup goal detection for this agent.
+        goalDetect = block.GetComponent<GoalDetect_ex>();
+        goalDetect.agent = this;
+
+        m_AgentRb = GetComponent<Rigidbody>();
+        m_BlockRb = GetComponent<Rigidbody>();
+
+        areaBounds = ground.GetComponent<Collider>().bounds;
+
+        m_GroundRenderer = ground.GetComponent<Renderer>();
+        m_GroundMaterial = m_GroundRenderer.material;
+
+        SetResetParameters();
+    }
+
+    public override float[] Heuristic() {
+        return base.Heuristic();
+    }
+
+    public override void CollectObservations() {
+        base.CollectObservations();
+    }
+
+    public override void AgentAction(float[] vectorAction, string textAction) {
+        base.AgentAction(vectorAction, textAction);
+    }
+
+    public void SetResetParameters() {
+        // TODO 
+    }
+}

e96191190b86b282e30f4e8715f30a627ee3fe72
Kyle Clarks <kyleclarkson17@hotmail.com>
2020-02-01 19:28:49 -0800

    Update FoodCollectorAcademy.cs
    Implemented FoodCollectorAcademy script.

--- ML-Agents-Examples/Assets/ML-Agents/Examples-Redone/FoodCollector/Scripts/FoodCollectorAcademy.cs
+++ ML-Agents-Examples/Assets/ML-Agents/Examples-Redone/FoodCollector/Scripts/FoodCollectorAcademy.cs
@@ -1,18 +1,49 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using MLAgents;
 
-public class FoodCollectorAcademy : MonoBehaviour
+public class FoodCollectorAcademy : Academy
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [HideInInspector]
+    public GameObject[] agents;
+
+    [HideInInspector]
+    public FoodCollectorArea[] listArea;
+
+    public int totalScore;
+    public Text scoreText;
+
+    /// <summary>
+    /// Reset areas. 
+    /// </summary>
+    public override void AcademyReset() {
+        ClearObjects(GameObject.FindGameObjectsWithTag("food"));
+        ClearObjects(GameObject.FindGameObjectsWithTag("badFood"));
+
+        agents = GameObject.FindGameObjectsWithTag("agent");
+        listArea = FindObjectsOfType<FoodCollectorArea>();
+        // Reset each area.
+        foreach (var area in listArea) {
+            area.ResetFoodArea(agents);
+        }
+
+        totalScore = 0;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    /// <summary>
+    /// Update score display.
+    /// </summary>
+    public override void AcademyStep() {
+        scoreText.text = string.Format(@"Score: {0}", totalScore);
+    }
+
+    /// <summary>
+    /// Remove any food items present.
+    /// </summary>
+    /// <param name="objects">Objects to be removed.</param>
+    void ClearObjects(GameObject[] objects) {
+        foreach(var food in objects) {
+            Destroy(food);
+        }
     }
 }

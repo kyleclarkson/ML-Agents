88b4950b77049322d0b0149b723037d1f92c92b6
Kyle Clarks <kyleclarkson17@hotmail.com>
2020-03-01 10:22:44 -0800

    BallRoll example progress.

--- ml-agents-0.14.1/examples/mla-examples/Assets/PracticeEnvs/BallRoll/Scripts/RollerAgent.cs
+++ ml-agents-0.14.1/examples/mla-examples/Assets/PracticeEnvs/BallRoll/Scripts/RollerAgent.cs
@@ -12,6 +12,7 @@ public class RollerAgent : Agent
 
     Rigidbody rBody;
     public Transform TargetBall;
+    public float agentSpeed;
 
     void Start()
     {
@@ -32,4 +33,42 @@ public class RollerAgent : Agent
             0.5f,
             Random.value * 8 - 4);
     }
+
+    // TODO This may need to be override instead of virutal. 
+    public virtual void CollectObservations(VectorSensor sensor) {
+
+        // Add target ball and agent's position
+        sensor.AddObservation(TargetBall.position);
+        sensor.AddObservation(this.transform.position);
+
+        // x, z velocity of agent
+        sensor.AddObservation(rBody.velocity.x);
+        sensor.AddObservation(rBody.velocity.z);
+    }
+
+    public override void AgentAction(float[] vectorAction) {
+
+        // 2 Actions: Force in X and Z direction.
+        Vector3 control = Vector3.zero;
+        control.x = vectorAction[0];
+        control.z = vectorAction[1];
+        rBody.AddForce(control * agentSpeed);
+
+        // Reward
+
+        float dstToTarget = Vector3.Distance(this.transform.position, TargetBall.position);
+
+        if (dstToTarget < 1.42f) {
+            // Close enough to target.
+            SetReward(1.0f);
+            Done();
+        }
+
+        if (this.transform.position.y < 0) {
+            // Fell off clift.
+            SetReward(-1.0f);
+            Done();
+        }
+    }
+
 }

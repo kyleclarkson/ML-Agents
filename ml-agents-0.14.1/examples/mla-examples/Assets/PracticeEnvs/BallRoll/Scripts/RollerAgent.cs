005f0886a36d0e896c4e1fb6fc1274d4a0b6322b
Kyle Clarks <kyleclarkson17@hotmail.com>
2020-02-29 23:24:25 -0800

    Created 0.14.1 folder.

---
+++ ml-agents-0.14.1/examples/mla-examples/Assets/PracticeEnvs/BallRoll/Scripts/RollerAgent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using MLAgents;
+using MLAgents.Sensor;
+
+
+
+public class RollerAgent : Agent
+{
+
+    Rigidbody rBody;
+    public Transform TargetBall;
+
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    public override void AgentReset() {
+        if(this.transform.position.y < 0) {
+            // Agent has fallen off plane. Reset agent.
+            this.rBody.angularVelocity = Vector3.zero;
+            this.rBody.velocity = Vector3.zero;
+            this.transform.position = new Vector3(0, 0.5f, 0);
+        }
+
+        // Target is reached. Reset its location. 
+        TargetBall.position = new Vector3(
+            Random.value * 8 - 4,
+            0.5f,
+            Random.value * 8 - 4);
+    }
+}

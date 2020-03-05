faa69a7123de6ad5462561c2297e382e8a73357c
Kyle Clarks <kyleclarkson17@hotmail.com>
2020-03-04 17:16:34 -0800

    Progress on FoodCollector example

---
+++ ml-agents-0.14.1/mla-examples/Assets/PracticeEnvs/FoodCollector/Scripts/FoodCollectorArea.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using MLAgentsExamples;
+
+namespace Examples {
+
+    public class FoodCollectorArea : Area {
+
+        public GameObject food;
+        public GameObject badFood;
+
+        public int numFood;
+        public int numBadFood;
+
+        public bool respawnFood;
+        public float range;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="num">The amount of food to create</param>
+        /// <param name="foodItem">The food object to be cloned</param>
+        public void CreateFood(int numOfFood, GameObject foodItem) {
+
+            for (int i=0; i < numOfFood; i++) {
+                GameObject food = Instantiate(foodItem, new Vector3(
+                    Random.Range(-range, range), 1f, Random.Range(-range, range)) + transform.position,
+                    Quaternion.Euler(new Vector3(0f, Random.Range(0f,360f), 90f)));
+
+                food.GetComponent<FoodLogic>().respawn = respawnFood;
+                food.GetComponent<FoodLogic>().myArea = this;
+            }
+        }
+
+
+        public void ResetFoodArea(GameObject agent) {
+            // TODO
+        }
+
+        public override void ResetArea() {
+            // Empty override method.
+        }
+    }
+}
---
+++ ml-agents-0.14.1/mla-examples/Assets/PracticeEnvs/FoodCollector/Scripts/FoodLogic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Examples {
+
+    public class FoodLogic : MonoBehaviour {
+
+        public bool respawn;
+        public FoodCollectorArea myArea;
+
+        public void OnEaten() {
+
+            // Food item will either respawn or not. 
+            if (respawn) {
+                transform.position = new Vector3(
+                    Random.RandomRange(-myArea.range, myArea.range),
+                    3f,
+                    Random.RandomRange(-myArea.range, myArea.range) + myArea.transform.position;
+            } else {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+}

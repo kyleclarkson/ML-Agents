ce7d53ec06be167af835b30e2a0e3482bd2c1b7f
Kyle Clarks <kyleclarkson17@hotmail.com>
2020-03-07 13:55:30 -0800

    Progess on WallJump example.
    Created scene. TODO agent class.

---
+++ ml-agents-0.14.1/mla-examples/Assets/PracticeEnvs/WallJump/Scripts/WallJumpSettings.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Examples {
+
+    public class WallJumpSettings : MonoBehaviour {
+
+        [Header("Sepcific to WallJump")]
+        public float agentRunSpeed;
+        public float agentJumpHeight;
+
+        // Ground will change color if goal is met/failed.
+        public Material goalMetMaterial;
+        public Material goalFailedMaterial;
+
+        [HideInInspector]
+        public float agentJumpVelocity = 777;
+        []HideInInspector]
+        public float agentJumpVelocityMaxChange = 10;
+    }
+}

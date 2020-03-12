2ce2c998e610f9664a7a2882133855c2baf1d30d
Kyle Clarks <kyleclarkson17@hotmail.com>
2020-03-11 22:33:53 -0700

    QueryAgent
    Implemented heurisitc and move methods. Will need to determine how to incorperate speed. Datapoints are also triggered.

--- ml-agents-0.14.1/mla-examples/Assets/MovingEntities/BasicOne/Scripts/QueryAgent.cs
+++ ml-agents-0.14.1/mla-examples/Assets/MovingEntities/BasicOne/Scripts/QueryAgent.cs
@@ -5,17 +5,75 @@ using MLAgents;
 
 public class QueryAgent : Agent {
 
-    public Rigidbody m_AgentRb;
+    Rigidbody m_AgentRb;
     public GameObject area;
-    public QueryPointSettings m_QueryPointSettings;
+    QueryPointArea m_MyArea;
+    QueryPointSettings m_QueryPointSettings;
 
     // Agent movement properties.
     public float turnSpeed = 300;
     public float moveSpeed = 2;
 
 
-    public override void AgentAction(float[] vectorAction) {
-        base.AgentAction(vectorAction);
+    public override void InitializeAgent() {
+
+        Debug.Log("Init agent called");
+        base.InitializeAgent();
+
+        m_AgentRb = GetComponent<Rigidbody>();
+        m_MyArea = area.GetComponent<QueryPointArea>();
+        m_QueryPointSettings = FindObjectOfType<QueryPointSettings>();
+    }
+
+
+    public override void AgentAction(float[] action) {
+
+        Debug.Log("agent action called");
+        // Move agent.
+        var dirToGo = Vector3.zero;
+        var rotateDir = Vector3.zero;
+
+        var forwardAxis = (int)action[0];
+        var rightAxis = (int)action[1];
+        var rotateAxis = (int)action[2];
+
+        switch (forwardAxis) {
+            case 1:
+                dirToGo = transform.forward;
+                break;
+            case 2:
+                dirToGo = -transform.forward;
+                break;
+        }
+
+        switch (rightAxis) {
+            case 1:
+                dirToGo = transform.right;
+                break;
+
+            case 2:
+                dirToGo = -transform.right;
+                break;
+        }
+
+        switch (rotateAxis) {
+            case 1:
+                rotateDir = transform.up;
+                break;
+            case 2:
+                rotateDir = -transform.up;
+                break;
+        }
+
+        // Apply movement and rotatio
+        m_AgentRb.AddForce(dirToGo * moveSpeed, ForceMode.VelocityChange);
+        transform.Rotate(rotateDir, Time.fixedDeltaTime * turnSpeed);
+
+        // Slow down agent if moving too fase.
+        if (m_AgentRb.velocity.sqrMagnitude > 20f) {
+            m_AgentRb.velocity *= 0.95f;
+        }
+
     }
 
     public override void AgentReset() {
@@ -26,15 +84,49 @@ public class QueryAgent : Agent {
         base.CollectObservations();
     }
 
+    /// <summary>
+    /// The 4 axis correspond to:
+    /// 0 - Moving forward/backward
+    /// 1 - Moving left/right
+    /// 2 - Rotating
+    /// 3 - Shooting laser
+    /// Player only has access to 0 and 2. 
+    /// </summary>
+    /// <returns></returns>
     public override float[] Heuristic() {
-        ///
 
-        ///
-        var action = new float[4];
+        Debug.Log("heuristic called");
+        var action = new float[3];
+
+        // Get KB input.
+        if (Input.GetKey(KeyCode.D)) {
+            action[2] = 2f;
+        }
+        if (Input.GetKey(KeyCode.W)) {
+            action[0] = 1f;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            action[2] = 1f;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            action[0] = 2f;
+        }
+        return action;
 
     }
 
-    public override void InitializeAgent() {
-        base.InitializeAgent();
+
+    // =========================
+    // === End Agent methods ===
+    // =========================
+    private void OnTrigger(Collision collision) {
+    }
+
+    private void OnTriggerEnter(Collider collision) {
+        // Collision with data point
+        if (collision.gameObject.CompareTag("data_point")) {
+            Debug.Log("Collision with: " + collision.gameObject.name);
+
+        }
     }
 }
